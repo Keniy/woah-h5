edd7123d0af734d2c3a1ca6de75aec688a5efdd4
liugenyuan <liugenyuan@zhihuijianshe.com>
2019-11-13 16:23:14 +0800

    updated

--- setup.sh
+++ setup.sh
@@ -1,17 +1,17 @@
 image_version=`date +%Y%m%d%H%M`;
-echo $image_version;
+echo v1.0;
 # cd woah-h5
 git pull --rebase origin master;
 docker stop woah-h5;
 docker rm woah-h5;
-docker build -t woah-h5:$image_version .;
+docker build -t woah-h5:v1.0 .;
 echo '---------build end-----------------'
-docker tag woah-h5:$image_version 49.233.192.53:5000/woah-h5:$image_version
+docker tag woah-h5:v1.0 49.233.192.53:5000/woah-h5:v1.0;
 echo '-----------docker tag end------------'
-docker push 49.233.192.53:5000/woah-h5:$image_version;
+docker push 49.233.192.53:5000/woah-h5:v1.0;
 echo '-----------docker push end------------'
 docker images;
-docker run -p 1010:80 -d --name woah-h5 woah-h5:$image_version;
+docker run -p 1010:80 -d --name woah-h5 woah-h5:v1.0;
 # -v ~/docker-data/house-web/appsettings.json:/app/appsettings.json -v ~/docker-data/house-web/NLogFile/:/app/NLogFile   --restart=always
 docker logs woah-h5;
 #删除build过程中产生的镜像    #docker image prune -a -f
