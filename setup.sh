d3a7b39c2b521b072d081ca6bb205b0393a059b6
liugenyuan <liugenyuan@zhihuijianshe.com>
2019-11-13 10:17:30 +0800

    updated

--- setup.sh
+++ setup.sh
@@ -2,12 +2,12 @@ image_version=`date +%Y%m%d%H%M`;
 echo $image_version;
 # cd woah-h5
 git pull --rebase origin master;
-docker stop woah-h5;
-docker rm woah-h5;
-docker build -t woah-h5:$image_version .;
-docker images;
-docker run -p 10002:80 -d --name woah-h5 woah-h5:$image_version;
+sudo docker stop woah-h5;
+sudo docker rm woah-h5;
+sudo docker build -t woah-h5:$image_version .;
+sudo docker images;
+sudo docker run -p 10002:80 -d --name woah-h5 woah-h5:$image_version;
 # -v ~/docker-data/house-web/appsettings.json:/app/appsettings.json -v ~/docker-data/house-web/NLogFile/:/app/NLogFile   --restart=always
-docker logs woah-h5;
+sudo docker logs woah-h5;
 #删除build过程中产生的镜像    #docker image prune -a -f
-docker rmi $(docker images -f "dangling=true" -q)
+sudo docker rmi $(docker images -f "dangling=true" -q)
