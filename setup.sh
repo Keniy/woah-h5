6e69d55c98705955ba326f5c09925a0f29a15aff
liugenyuan <liugenyuan@zhihuijianshe.com>
2019-11-13 14:01:59 +0800

    updated

--- setup.sh
+++ setup.sh
@@ -2,9 +2,10 @@ image_version=`date +%Y%m%d%H%M`;
 echo $image_version;
 # cd woah-h5
 git pull --rebase origin master;
-docker stop woah-h5;
-docker rm woah-h5;
+# docker stop woah-h5;
+# docker rm woah-h5;
 docker build -t woah-h5:$image_version .;
+echo '---------build end-----------------'
 docker images;
 docker run -p 10002:80 -d --name woah-h5 woah-h5:$image_version;
 # -v ~/docker-data/house-web/appsettings.json:/app/appsettings.json -v ~/docker-data/house-web/NLogFile/:/app/NLogFile   --restart=always
