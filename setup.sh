b33868a3880eb14645829719d370439ad63a88ca
liugenyuan <liugenyuan@zhihuijianshe.com>
2019-11-28 13:31:20 +0800

    new commit

--- setup.sh
+++ setup.sh
@@ -2,11 +2,12 @@ image_version=`date +%Y%m%d%H%M`;
 echo v1.0;
 # cd woah-h5
 git pull --rebase origin master;
+docker images;
+echo '-----------docker images --------------'
 docker stop woah-h5;
 docker rm woah-h5;
 docker build -t woah-h5 .;
 echo '---------build end-----------------'
-docker images;
 docker run -p 8088:80 -d --name woah-h5 -v /home/jenkins_01/workspace/woah-h5/dist:/usr/share/nginx/html -v /home/jenkins_01/workspace/woah-h5/nginx/nginx.conf:/etc/nginx/nginx.conf woah-h5
 # -v ~/docker-data/house-web/appsettings.json:/app/appsettings.json -v ~/docker-data/house-web/NLogFile/:/app/NLogFile   --restart=always
 docker logs woah-h5;
